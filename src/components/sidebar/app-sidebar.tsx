c6ace7c167eae7926ea5c108f504dd1dbbbc7d5f
Kaung Satt Hein <kaungsatthein2742@gmail.com>
2025-03-17 10:16:39 +0630

    [ENHANCEMENT] change logo

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -1,31 +1,41 @@
-'use client'
+"use client";
 
 import {
   Sidebar,
   SidebarContent,
   SidebarHeader,
-} from "@/components/ui/sidebar"
-import { Fragment } from "react"
-import { NavLink } from "./nav-link"
-import { Separator } from "../ui/separator"
-import { sideNav } from "@/constants/side-nav"
-import Image from "next/image"
+} from "@/components/ui/sidebar";
+import { Fragment } from "react";
+import { NavLink } from "./nav-link";
+import { Separator } from "../ui/separator";
+import { sideNav } from "@/constants/side-nav";
+import Image from "next/image";
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader>
-        <div className="flex items-center gap-x-2 py-2 ps-4">
-        <Image
+        <div className="flex items-center justify-center gap-x-2 py-2 ">
+          {/* <Image
             src="vercel.svg"
             width={100}
             height={100}
             alt="Vercel"
             className="size-8"
-          />
-          <p className="truncate text-left text-md font-semibold leading-tight  group-data-[co700llapsible=icon]:hidden">
-            ADMIN PORTAL
-          </p>
+          /> */}
+          <div className="flex items-center gap-2">
+            <div className="relative h-6 w-6">
+              <Image
+                src="/logo.svg"
+                fill
+                alt="Univision Logo"
+                className="object-contain"
+              />
+            </div>
+            <p className="truncate text-lg text-primary font-semibold leading-tight group-data-[collapsible=icon]:hidden">
+              UNIVISION PORTAL
+            </p>
+          </div>
         </div>
       </SidebarHeader>
       <SidebarContent>
@@ -39,5 +49,5 @@ export function AppSidebar() {
         ))}
       </SidebarContent>
     </Sidebar>
-  )
+  );
 }
