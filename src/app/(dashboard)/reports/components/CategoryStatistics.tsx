47a7e417985e99f9e91f6e28c02bd527fad7136c
Kaung Satt Hein <kaungsatthein2742@gmail.com>
2025-04-07 16:33:15 +0630

    {reports dashboard}

---
+++ src/app/(dashboard)/reports/components/CategoryStatistics.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { format } from "date-fns";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+const chartData = [
+  { month: "Sale", desktop: 86 },
+  { month: "Marketing", desktop: 305 },
+  { month: "Product", desktop: 237 },
+  { month: "Development", desktop: 73 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Category",
+  },
+} satisfies ChartConfig;
+
+export default function CategoryStatistics() {
+  const currentDate = format(new Date(), "dd MMMM,yyyy");
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle className="font-bold text-xl text-primary">
+          Category Statistics
+        </CardTitle>
+        <CardDescription>{currentDate}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div>
+          <ChartContainer config={chartConfig}>
+            <BarChart accessibilityLayer data={chartData}>
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                tickMargin={10}
+                axisLine={false}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Bar dataKey="desktop" fill="hsl(217 100% 46%)" radius={5} />
+            </BarChart>
+          </ChartContainer>
+        </div>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 font-medium leading-none">
+          Category is created by QA Manager
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total category posts.
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ src/app/(dashboard)/reports/components/KeyMetrics.tsx
@@ -0,0 +1,58 @@
+import { Users, MessageSquare, ThumbsUp, Lightbulb } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+
+const KeyMetrics = () => {
+  return (
+    <>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 ">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Total Ideas</CardTitle>
+            <Lightbulb className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">54</div>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Thumbs Up/Down
+            </CardTitle>
+            <ThumbsUp className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">128/67</div>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Total Comments
+            </CardTitle>
+            <MessageSquare className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">237</div>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Active Users</CardTitle>
+            <Users className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">573</div>
+          </CardContent>
+        </Card>
+      </div>
+      {/* <Separator /> */}
+    </>
+  );
+};
+
+export default KeyMetrics;
