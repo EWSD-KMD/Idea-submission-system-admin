05262b2f3087b7ddf0a9631e9ba4153b646c052f
Nyan Ye Htet <nyanyairhtet@ayainnovations.com>
2025-04-19 19:58:58 +0630

    fix user create and update form

--- src/types/idea.ts
+++ src/types/idea.ts
@@ -1,8 +1,30 @@
+import { AcademicYear } from "./academic-year";
+import { Category } from "./category";
+import { Department } from "./department";
+import { User } from "./user";
+
+interface Comment {
+  id: number;
+  content: string;
+  ideaId: number;
+  anonymous: boolean;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
+  status: string;
+  user: User;
+}
+
+interface Count {
+  comments: number;
+  reports: number;
+}
+
 export interface Idea {
   id: number;
   title: string;
   description: string;
-  status: "SHOW" | "HIDE" | string;
+  status: string;
   anonymous: boolean;
   categoryId: number;
   departmentId: number;
@@ -12,7 +34,16 @@ export interface Idea {
   dislikes: number;
   views: number;
   createdAt: string;
-  updatedAt: string; 
+  updatedAt: string;
+  category: Category;
+  department: Department;
+  academicYear: AcademicYear;
+  user: User;
+  files: [];
+  comments: Comment[];
+  _count: Count;
+  likeInd: boolean;
+  dislikeInd: boolean;
 }
 
 
